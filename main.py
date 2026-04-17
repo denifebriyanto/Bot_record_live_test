8152b5015ca8037bcfb66c996804c519fce72ce6
Rybira <70983370+denifebriyanto@users.noreply.github.com>
2026-04-18 02:49:22 +0700

    Update main.py

--- main.py
+++ main.py
@@ -26,11 +26,8 @@ def get_stream():
         cmd = [
             "yt-dlp",
             "--no-warnings",
-            "--impersonate", "chrome",
             "--geo-bypass",
             "--no-check-certificates",
-            "--user-agent",
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64)",
             "-f",
             "best",
             "-g",

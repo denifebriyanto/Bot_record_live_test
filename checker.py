eea95ac58e339689c52f6543e20aada4475940fb
Rybira <70983370+denifebriyanto@users.noreply.github.com>
2026-04-21 00:35:46 +0700

    Update checker.py

--- checker.py
+++ checker.py
@@ -1,38 +1,17 @@
+import asyncio
 from TikTokLive import TikTokLiveClient
-from TikTokLive.client.errors import UserOfflineError, UserNotFoundError
 
-async def is_live(username: str):
+async def is_live(username):
     try:
         client = TikTokLiveClient(unique_id=username)
-        await client.retrieve_room_info()
 
-        if client.room_info and client.room_info.get("status") == 4:
-            stream_url = await get_stream_url(client)
-            return True, stream_url
+        await client.start()
 
-        return False, None
+        stream_url = client.webcast.stream_url
 
-    except UserOfflineError:
-        return False, None
-    except UserNotFoundError:
-        return False, None
-    except Exception as e:
-        print(f"Error cek @{username}: {e}")
-        return False, None
+        await client.disconnect()
 
-
-async def get_stream_url(client):
-    try:
-        info = client.room_info
-
-        stream = info.get("stream_url", {})
-
-        hls = (
-            stream.get("hls_pull_url")
-            or stream.get("hls_pull_url_map", {}).get("SD1")
-        )
-
-        return hls
+        return True, stream_url
 
     except Exception:
-        return None
+        return False, None
